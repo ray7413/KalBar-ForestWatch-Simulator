6f836dba42023d376f443c5c76eef4709193f7f2
ray7413 <88642760+ray7413@users.noreply.github.com>
2026-08-23 16:41:20 +0700

    auto-commit

--- main.py
+++ main.py
@@ -12,18 +12,52 @@ pygame.display.set_caption("KalBar ForestWatch Simulator")
 clock = pygame.time.Clock()
 
 load()
+
 forest_map = forestMap()
 forest_map.generate()
 
+mx, my, mx0, my0, dmx, dmy = 0
+
+
 running = True
+
 while running:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
+
             running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+
+            if event.button == 1:
+
+                left_click = True
+
+            elif event.button == 3:
+
+                right_click = True
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+
+            if event.button == 1:
+            
+                left_click = False
+
+            elif event.button == 3:
+
+                right_click = False
+
+        elif event.type == pygame.MOUSEMOTION:
+
+            mx, my = event.pos
+
+            
 
     forest_map.render(screen)
 
     pygame.display.flip()
-    clock.tick(99)
+    clock.tick(60)
 
 pygame.quit()
--- map.py
+++ map.py
@@ -5,26 +5,44 @@ from assets.asset import stamp
 WIDTH = 20
 HEIGHT = 20
 
+TILE_WIDTH = 64
+TILE_HEIGHT = 32
+
 def grid_to_screen(x, y, offset_x=0, offset_y=0):
-    screen_x = (x - y) * (64 // 2) + offset_x
-    screen_y = (x + y) * (32 // 2) + offset_y
+
+    screen_x = (x - y) * (TILE_WIDTH // 2) + offset_x
+    screen_y = (x + y) * (TILE_HEIGHT // 2) + offset_y
+
     return screen_x, screen_y
 
 class forestMap:
 
     def __init__(self):
+
         self.tiles = []
 
+        self.offsetx = 200
+        self.offsety = 50
+
     def generate(self):
+
         for y in range(HEIGHT):
+
             columns = []
+
             for x in range(WIDTH):
+
                 columns.append("forest")
+
             self.tiles.append(columns)
 
     def render(self, screen):
+
         for y in range(len(self.tiles)):
+
             for x in range(len(self.tiles[y])):
+
                 tile_type = self.tiles[y][x]
+
                 screen_x, screen_y = grid_to_screen(x, y, 50, 50)
                 stamp(screen, tile_type, screen_x, screen_y)
